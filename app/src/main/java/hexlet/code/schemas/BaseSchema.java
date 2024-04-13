1a724343c460ac8c1d46af48fb3b031572a019db
Eugene <braincucumber@mail.ru>
2024-04-13 14:51:41 +0300

    codestyle fixes

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -17,7 +17,8 @@ public abstract class BaseSchema<T> {
     }
 
     /**
-     * Check if value is null or empty string
+     * Check if value is null or empty string.
+     * @return Schema
     */
     public BaseSchema<T> required() {
         addValidation("required", o -> o instanceof String ? o != null && !o.equals("") : o != null);
